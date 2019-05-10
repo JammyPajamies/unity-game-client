fd40bfa5bcc2aebcb49282eeb057bc760fbe7356
Marlo Sandoval <darkarceus2@gmail.com>
2019-05-09 21:08:31 -0700

    Minor changes

--- Assets/SDAssets/Scripts/Scenes/SDGameMain/Buffs&Debuffs/BuffEffects.cs
+++ Assets/SDAssets/Scripts/Scenes/SDGameMain/Buffs&Debuffs/BuffEffects.cs
@@ -38,14 +38,29 @@ namespace SD
 
         IEnumerator activateEffect()
         {
-            //speed power-up
             if (gameController.getSpeedBoostStatus() == true)
             {
                 //changes player model's color when power-up is picked up
                 meshRenderer.material.SetColor("_Color", Color.blue);
 
+                // Get trails and enable them.
+                Component[] buffTrails = this.GetComponentsInChildren<TrailRenderer>();
+                foreach (TrailRenderer trail in buffTrails)
+                {
+                    if (!trail.isVisible)
+                    {
+                        trail.enabled = true;
+                    }
+                }
+
                 //duration of power-up's effects
-                yield return new WaitForSeconds(speedBoostDuration);
+                yield return new WaitForSeconds(3f);
+
+                // Disable the extra trails.
+                foreach (TrailRenderer trail in buffTrails)
+                {
+                    trail.enabled = false;
+                }
 
                 //returns player model color back to original
                 //sets power-up status to false
