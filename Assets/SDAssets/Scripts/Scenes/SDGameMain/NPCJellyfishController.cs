6307d586d70273c056a436bc1d63550d109b14d7
Marlo Sandoval <darkarceus2@gmail.com>
2019-05-09 20:17:37 -0700

    Minor model changes

--- Assets/SDAssets/Scripts/Scenes/SDGameMain/NPCJellyfishController.cs
+++ Assets/SDAssets/Scripts/Scenes/SDGameMain/NPCJellyfishController.cs
@@ -7,24 +7,38 @@ Description: This script handles the movement for the two jellyfish species.
 Each species moves differently. 
 */
 
-public class NPCJellyfishController : MonoBehaviour {
+public class NPCJellyfishController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start()
+    {
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //standard jellyfish movement
-        if (gameObject.name== "NpcJellyfish") {
-            transform.Translate(0f, 1f * Time.deltaTime, 0f);
-        }
 
-        //man-o-war movement
-        if (gameObject.name == "NpcManOWar")
+    // Update is called once per frame
+    void Update()
+    {
+        //standard jellyfish movement
+        if (gameObject.name == "NpcJellyfish")
         {
-            transform.Translate(.5f * Time.deltaTime, 0f, 0f);
+            transform.Translate(0f, 15f * Time.deltaTime, 0f);
+            if (transform.position.y <= -165)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, -45);
+            }
+            if (transform.position.y >= 165)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, -135);
+            }
+            if (transform.position.x >= 450)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 135);
+            }
+            if (transform.position.x <= -450)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, -135);
+            }
         }
     }
 }
